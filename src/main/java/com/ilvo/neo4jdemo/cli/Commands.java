c24d478ec1a76d1fbeaefdd116c369a98409b097
Eternal-light-blip <2314181884@qq.com>
2023-01-27 22:51:28 +0800

    Command用Service层替换Repository

--- src/main/java/com/ilvo/neo4jdemo/cli/Commands.java
+++ src/main/java/com/ilvo/neo4jdemo/cli/Commands.java
@@ -12,55 +12,52 @@ import com.ilvo.neo4jdemo.dto.ActedInProperty;
 import com.ilvo.neo4jdemo.dto.ReviewedProperties;
 import com.ilvo.neo4jdemo.nodes.Movie;
 import com.ilvo.neo4jdemo.nodes.Person;
-import com.ilvo.neo4jdemo.repository.MovieRepository;
-import com.ilvo.neo4jdemo.repository.PersonRepository;
+import com.ilvo.neo4jdemo.nodes.properties.BornYear;
+import com.ilvo.neo4jdemo.nodes.properties.Name;
+import com.ilvo.neo4jdemo.nodes.properties.ReleasedYear;
+import com.ilvo.neo4jdemo.nodes.properties.TagLine;
+import com.ilvo.neo4jdemo.nodes.properties.Title;
+import com.ilvo.neo4jdemo.relationships.properties.Rating;
+import com.ilvo.neo4jdemo.relationships.properties.Roles;
+import com.ilvo.neo4jdemo.relationships.properties.Summary;
+import com.ilvo.neo4jdemo.service.PersonService;
+import com.ilvo.neo4jdemo.service.MovieService;
 
 @ShellComponent
 public class Commands {
     
-    final private Terminal terminal;
-    final private PersonRepository personRepository;
-    final private MovieRepository movieRepository;
+    private final Terminal terminal;
+    private final PersonService personService;
+    private final MovieService movieService;
     
-    public Commands(Terminal terminal,PersonRepository personRepository,MovieRepository movieRepository){
+    public Commands(Terminal terminal,PersonService personService, MovieService MovieService){
         this.terminal = terminal;
-        this.personRepository = personRepository;
-        this.movieRepository = movieRepository; 
+        this.personService = personService;
+        this.movieService = MovieService; 
     }
     
     @Transactional
     @ShellMethod(value = "add a person node,you should input name then born year",group = "Node Basic Commands")
     public void addPerson(String name, Integer bornYear){
-
-        Person person = new Person(name,bornYear);
-        personRepository.save(person);
-
-        person = personRepository.findByName(person.getName());
-        terminal.writer().println("you add the person :\n"+person.infomation());
-
+        
+        personService.addPerson(Name.of(name), BornYear.of(bornYear));
+        terminal.writer().println("you add the person :"+name);
     }
     
     @Transactional
     @ShellMethod(value = "delete a person node,you should input name",group = "Node Basic Commands")
     public void deletePerson(String name){
 
-        Person person = personRepository.findByName(name);
-        
-        personRepository.delete(person);
-        terminal.writer().println("you delete the person :\n"+person.infomation());
-
+        personService.deletePerson(Name.of(name));
+        terminal.writer().println("you delete the person :"+name);
     }
 
     @Transactional
     @ShellMethod(value = "add a movie node,you should input title, tagline then released year",group = "Node Basic Commands")
     public void addMovie(String title, String tagline, Integer releasedYear){
 
-        //Movie movie = new Movie(title,tagline,releasedYear);
-        Movie movie = new Movie(title,tagline,releasedYear);
-        movieRepository.save(movie);
-
-        movie = movieRepository.findByTitle(movie.getTitle());
-        terminal.writer().println("you add the movie :\n"+ movie.infomation());
+        movieService.addMovie(Title.of(title),TagLine.of(tagline),ReleasedYear.of(releasedYear));
+        terminal.writer().println("you add the movie :"+ title);
 
     }
 
@@ -68,10 +65,8 @@ public class Commands {
     @ShellMethod(value = "delete a movie node,you should input title",group = "Node Basic Commands")
     public void deleteMovie(String title){
 
-        Movie movie = movieRepository.findByTitle(title);
-
-        movieRepository.delete(movie);
-        terminal.writer().println("you delete the movie :\n"+movie.infomation());
+        movieService.deleteMovie(Title.of(title));
+        terminal.writer().println("you delete the movie :"+title);
 
     }
 
@@ -79,12 +74,7 @@ public class Commands {
     @ShellMethod(value = "add a ACTED_In relationship, you should input person's name, movie's title then the roles",group = "Relationship Basic Commands")
     public void actedIn(String name, String title, String[] roles){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.actedIn(movie, roles);
-        personRepository.save(person);
-
+        personService.actedIn(Name.of(name),Title.of(title),Roles.of(roles));
         terminal.writer().println("you add an ACTED_IN relationship between "+name+" and "+title);
         
     }
@@ -93,11 +83,7 @@ public class Commands {
     @ShellMethod(value = "delete a ACTED_In relationship, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void deleteActedIn(String name, String title){
         
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.deleteActedIn(movie);
-        personRepository.save(person);
+        personService.deleteActedIn(Name.of(name),Title.of(title));
         terminal.writer().println("you delete an ACTED_IN relationship between "+name+" and "+title);
         
     }
@@ -106,11 +92,7 @@ public class Commands {
     @ShellMethod(value = "add a REVIEWED relationship, you should input person's name, movie's title, summary and rating",group = "Relationship Basic Commands")
     public void reviewed(String name, String title, String summaty, Integer rating){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.reviewed(movie, summaty,rating);
-        personRepository.save(person);
+        personService.reviewed(Name.of(name),Title.of(title),Summary.of(summaty),Rating.of(rating));
         terminal.writer().println("you add an REVIEWED relationship between "+name+" and "+title);
     
     }
@@ -119,11 +101,7 @@ public class Commands {
     @ShellMethod(value = "delete a REVIEWED relationship, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void deleteReviewed(String name, String title){
        
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.deleteReviewed(movie);
-        personRepository.save(person);
+        personService.deleteReviewed(Name.of(name),Title.of(title));
         terminal.writer().println("you delete an REVIEWED relationship between "+name+" and "+title);
 
     }
@@ -132,11 +110,7 @@ public class Commands {
     @ShellMethod(value = "add a FOLLOWS relationship, you should input follower's name, master's name",group = "Relationship Basic Commands")
     public void follows(String name_follower, String name_master){
 
-        Person follower = personRepository.findByName(name_follower);
-        Person master = personRepository.findByName(name_master);
-
-        follower.follows(master);
-        personRepository.save(follower);
+        personService.follows(Name.of(name_follower),Name.of(name_master));
         terminal.writer().println("you add an FOLLOWS relationship between "+name_follower+" and "+name_master);
 
     }
@@ -145,11 +119,7 @@ public class Commands {
     @ShellMethod(value = "delete a FOLLOWS relationship, you should input follower's name, master's name",group = "Relationship Basic Commands")
     public void deleteFollows(String name_follower, String name_master){
         
-        Person follower = personRepository.findByName(name_follower);
-        Person master = personRepository.findByName(name_master);
-        
-        follower.deleteFollows(master);
-        personRepository.save(follower);
+        personService.deleteFollows(Name.of(name_follower),Name.of(name_master));
         terminal.writer().println("you delete an FOLLOWS relationship between "+name_follower+" and "+name_master);
 
     }
@@ -158,11 +128,7 @@ public class Commands {
     @ShellMethod(value = "add a DIRECTED relationship, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void directed(String name, String title){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.directed(movie);
-        personRepository.save(person);
+        personService.directed(Name.of(name),Title.of(title));
         terminal.writer().println("you add an DIRECTED relationship between "+name+" and "+title);
 
     }
@@ -171,11 +137,7 @@ public class Commands {
     @ShellMethod(value = "delete a DIRECTED relationship, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void deleteDirected(String name, String title){
         
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-        
-        person.deleteDirected(movie);
-        personRepository.save(person);
+        personService.deleteDirected(Name.of(name),Title.of(title));
         terminal.writer().println("you delete an DIRECTED relationship between "+name+" and "+title);
 
     }
@@ -184,11 +146,7 @@ public class Commands {
     @ShellMethod(value = "add a PRODUCED relationship, you should input person's name , movie's title",group = "Relationship Basic Commands")
     public void produced(String name, String title){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.produced(movie);
-        personRepository.save(person);
+        personService.produced(Name.of(name),Title.of(title));
         terminal.writer().println("you add an PRODUCED relationship between "+name+" and "+title);
 
     }
@@ -197,11 +155,7 @@ public class Commands {
     @ShellMethod(value = "delete a PRODUCED relationship, you should input person's name , movie's title",group = "Relationship Basic Commands")
     public void deleteProduced(String name, String title){
          
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-        
-        person.deleteProduced(movie);
-        personRepository.save(person);
+        personService.deleteProduced(Name.of(name),Title.of(title));
         terminal.writer().println("you delete an PRODUCED relationship between "+name+" and "+title);
 
     }
@@ -210,11 +164,7 @@ public class Commands {
     @ShellMethod(value = "add a WROTE relationship, you should input person's name , movie's title",group = "Relationship Basic Commands")
     public void wrote(String name, String title){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        person.wrote(movie);
-        personRepository.save(person);
+        personService.wrote(Name.of(name),Title.of(title));
         terminal.writer().println("you add an WROTE relationship between "+name+" and "+ title);
 
     }
@@ -223,11 +173,7 @@ public class Commands {
     @ShellMethod(value = "delete a WROTE relationship, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void deleteWrote(String name, String title){
         
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-        
-        person.deleteWrote(movie);
-        personRepository.save(person);
+        personService.deleteWrote(Name.of(name),Title.of(title));
         terminal.writer().println("you delete an WROTE relationship between "+name+" and "+title);
 
     }
@@ -235,15 +181,12 @@ public class Commands {
     @ShellMethod(value = "query property:roles of ACTED_IN, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void queryPropertiesActedIn(String name, String title){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        ActedInProperty actedInProperty = person.getActedInProperty(movie);
-        terminal.writer().println("the roles of ActedIn relationship between "+name+" and "+title+" :");
+        ActedInProperty actedInProperty =  personService.getActedInProperty(Name.of(name),Title.of(title));
+        terminal.writer().println("the roles of ActedIn relationship between "+name+" and "+title+" : ");
 			String[] roles = actedInProperty.getRoles();
 			int length = roles.length;
 			for(int i=0;i< length;i++){
-				terminal.writer().println(roles[i]);
+				terminal.writer().print(roles[i]+" ");
 			}
 
     }
@@ -251,86 +194,83 @@ public class Commands {
     @ShellMethod(value = "query property:summary,rating of REVIEWED, you should input person's name, movie's title",group = "Relationship Basic Commands")
     public void queryPropertiesReviewed(String name, String title){
 
-        Person person = personRepository.findByName(name);
-        Movie movie = movieRepository.findByTitle(title);
-
-        ReviewedProperties reviewedProperties = person.getReviewedProperties(movie);
+        ReviewedProperties reviewedProperties =  personService.getReviewedProperties(Name.of(name),Title.of(title));
         terminal.writer().println("the summary of Reviewed relationship between "+name+" and "+title+" :");
-		terminal.writer().println(reviewedProperties.getSummary());
+		terminal.writer().print(reviewedProperties.getSummary());
 		terminal.writer().println("the rating of Reviewed relationship between "+name+" and "+title+" :");
-		terminal.writer().println(reviewedProperties.getRating().toString());
+		terminal.writer().print(reviewedProperties.getRating().toString());
 
     }
 
     @ShellMethod(value = "query movies which person acted in, you should input person's name",group = "Advanced Query Commands")
     public void queryActedInMovies(String name){
 
-		List<Movie> actedInMovies = movieRepository.findActedInMovies(name);
+		List<Movie> actedInMovies =  personService.getActedInMovies(Name.of(name));
 		terminal.writer().println("Movies acted by "+name+" :");
-		actedInMovies.forEach(movie->terminal.writer().println(movie.infomation()));
+		actedInMovies.forEach(movie->terminal.writer().println(movie));
 
     }
 
     @ShellMethod(value = "query movies which person reviewed, you should input person's name",group = "Advanced Query Commands")
     public void queryReviewedMovies(String name){
 
-		List<Movie> reviewedMovies = movieRepository.findReviewedMovies(name);
+		List<Movie> reviewedMovies =  personService.getReviewedMovies(Name.of(name));
 		terminal.writer().println("Movies reviewed by "+name+" :");
-		reviewedMovies.forEach(movie->terminal.writer().println(movie.infomation()));
+		reviewedMovies.forEach(movie->terminal.writer().println(movie));
 
     }
 
     @ShellMethod(value = "query masters of person, you should input person's name",group = "Advanced Query Commands")
     public void queryMasters(String name){
 
-		List<Person> masters = personRepository.findMasters(name);
+		List<Person> masters =  personService.getMasters(Name.of(name));
 		terminal.writer().println("Masters followed by "+name+" :");
-		masters.forEach(person->terminal.writer().println(person.infomation()));
+		masters.forEach(person->terminal.writer().println(person));
 
     }
 
     @ShellMethod(value = "query followers of person, you should input person's name",group = "Advanced Query Commands")
     public void queryFollowers(String name){
 
-        List<Person> followers = personRepository.findFollowers(name);
+        List<Person> followers =  personService.getFollowers(Name.of(name));
 		terminal.writer().println("Followers of "+name+" :");
-		followers.forEach(person->terminal.writer().println(person.infomation()));
+		followers.forEach(person->terminal.writer().println(person));
 
     }
 
     @ShellMethod(value = "query movies which person directed, you should input person's name",group = "Advanced Query Commands")
     public void queryDirectedMovies(String name){
 
-		List<Movie> directedMovies = movieRepository.findDirectedMovies(name);
+		List<Movie> directedMovies =  personService.getDirectedMovies(Name.of(name));
 		terminal.writer().println("Movies directed by "+name+" :");
-		directedMovies.forEach(movie->terminal.writer().println(movie.infomation()));
+		directedMovies.forEach(movie->terminal.writer().println(movie));
 
     }
 
     @ShellMethod(value = "query movies which person produced, you should input person's name",group = "Advanced Query Commands")
     public void queryProducedMovies(String name){
 
-		List<Movie> producedMovies = movieRepository.findProducedMovies(name);
+		List<Movie> producedMovies =  personService.getProducedMovies(Name.of(name));
 		terminal.writer().println("Movies produced by "+name+" :");
-		producedMovies.forEach(movie->terminal.writer().println(movie.infomation()));
+		producedMovies.forEach(movie->terminal.writer().println(movie));
 
     }
 
     @ShellMethod(value = "query movies which person wrote, you should input person's name",group = "Advanced Query Commands")
     public void queryWrittenMovies(String name){
 
-		List<Movie> writtenMovies = movieRepository.findWrittenMovies(name);
+		List<Movie> writtenMovies =  personService.getWrittenMovies(Name.of(name));
 		terminal.writer().println("Movies written by "+name+" :");
-		writtenMovies.forEach(movie->terminal.writer().println(movie.infomation()));
+		writtenMovies.forEach(movie->terminal.writer().println(movie));
 
     }
 
     @ShellMethod(value = "query all relative people of movie, you should input movie's title",key = "query-relative-people-movie",group = "Advanced Query Commands")
     public void queryRelativePeopleOfMovie(String title){
 
-        List<Person> allRelativePeople = personRepository.findAllRelativePeopleOfMovie(title);
+        List<Person> allRelativePeople = movieService.getRelativePeople(Title.of(title));
 		terminal.writer().println("All relative people of the movie The Greeb Mile :");
-		allRelativePeople.forEach(person->terminal.writer().println(person.infomation()));
+		allRelativePeople.forEach(person->terminal.writer().println(person));
         
     }
 
